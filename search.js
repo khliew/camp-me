f608d90f4721221049cb08183b53df16d2ef866b
Kenny Liew <khl.swift@gmail.com>
2017-06-26 03:00:56 -0700

    add specific campground support

--- search.js
+++ search.js
@@ -40,52 +40,6 @@ function scheduleRefresh() {
 };
 
 /**
- * Inserts a countdown into the page until the next refresh.
- */
-function insertCountdown(endTime) {
-  var html = `
-    <div style='position: fixed; bottom: 16px; left: 16px; width: 112px; height: 64px; background-color: lightgray; box-shadow: 0 0 1em gray;'>
-      <div style='position: absolute; left: 8px; top: 8px'>
-        <label>Camp Me</label>
-        <br>
-        <span>Time to refresh:</span>
-        <br>
-        <b id='ce_time'>${toTimeText((endTime - Date.now()) / 1000)}</b>
-      </div>
-    </div>
-  `;
-  document.body.insertAdjacentHTML('beforeend', html);
-
-  var time = document.getElementById('ce_time');
-
-  var intervalId = setInterval(function() {
-    var interval = (endTime - Date.now()) / 1000;
-    time.textContent = toTimeText(interval);
-
-    if (interval < 0) {
-        clearInterval(intervalId);
-    }
-  }, 1000);
-}
-
-/**
- * Converts a time (in seconds) into a readable text.
- */
-function toTimeText(time) {
-  min = parseInt(time / 60, 10);
-  sec = parseInt(time % 60, 10);
-
-  if (min < 10) {
-    min = '0' + min;
-  }
-  if (sec < 10) {
-    sec = '0' + sec;
-  }
-
-  return min + ':' + sec;
-}
-
-/**
  * Performs a search.
  */
 function doSearch() {
